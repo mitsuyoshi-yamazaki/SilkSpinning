c61c994f9e7adc305c526429a15ba1ebfe7085a6
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2019-05-07 19:21:33 +0900

    Install article-parser

--- main.js
+++ main.js
@@ -2,3 +2,18 @@ var repeat = require('repeat-string');
 
 console.log(repeat('hey', 100));
 document.write(repeat('hey', 10));
+
+const {
+    extract 
+} = require('article-parser');
+   
+let url = 'https://goo.gl/MV8Tkh';
+
+console.log(extract);
+console.log('fuga');
+   
+extract(url).then((article) => {
+  console.log(article);
+}).catch((err) => {
+  console.log(err);
+});
